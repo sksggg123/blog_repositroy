a049852626e4a1c025481ebeae03b1b0db6774df
권병윤 <sksggg123@gmail.com>
2019-04-28 13:16:44 +0900

    변경 분 커밋 & 푸시

---
+++ boot-vue/frontend/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import App from '../App';
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path: '/',
+      name: 'App',
+      component: App
+    },
+  ]
+})
